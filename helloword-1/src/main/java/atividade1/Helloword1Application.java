f61e9e97f61466c9659e13e4891454c0ea631545
gabriel8514 <gabrielluizreis936@hotmail.com>
2021-07-23 15:15:03 -0300

    Atividades SpringBoot 23/07/2021

---
+++ helloword-1/src/main/java/atividade1/Helloword1Application.java
@@ -0,0 +1,13 @@
+package atividade1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloword1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helloword1Application.class, args);
+	}
+
+}
